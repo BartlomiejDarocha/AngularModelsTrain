3ed1e4a99a53669255ac44d7d3bb46478b0e0459
BartlomiejDarocha <bartlomiejdarocha@gmail.com>
2019-11-22 20:07:58 +0100

    dodanie styli bootstrapa, oraz przykladowy data picer

--- angularMaterials/src/app/app.component.ts
+++ angularMaterials/src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
-
+import { NgbDateStruct, NgbCalendar } from '@ng-bootstrap/ng-bootstrap';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
 export class AppComponent {
-  title = 'AngularMaterials';
+  public title = 'AngularMaterials';
+  // data NgbDateStruct model jaki przyjmuje data czyli : {year: number, month: number: day: number}
+  public dataModel: NgbDateStruct;
+  // po to by funkcja navigateTo mogla dopisać zmienie do pol w obiekcie
+  public date: { year: number, month: number };
+
+  constructor(
+    private calendar: NgbCalendar,
+  ) {}
+
+   public selectToday() {
+    // calendar service z przydatnymi funkcjami,
+    this.dataModel = this.calendar.getToday();
+    console.log(this.dataModel, 'dataModel');
+  }
+
 }
