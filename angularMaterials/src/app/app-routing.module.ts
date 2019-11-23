1b881361ad454f5c6d9cfefb6255a7e29f6b6714
BartlomiejDarocha <bartlomiejdarocha@gmail.com>
2019-11-23 22:52:56 +0100

    routing ready

--- angularMaterials/src/app/app-routing.module.ts
+++ angularMaterials/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FrutsListComponent } from './components/fruts/fruts-list/fruts-list.component';
+import { AppleComponent } from './components/fruts/apple/apple.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'fruts-list', component: FrutsListComponent},
+  {path: 'apple', component: AppleComponent},
+  {path: '', redirectTo: '/fruts-list' , pathMatch: 'full'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
