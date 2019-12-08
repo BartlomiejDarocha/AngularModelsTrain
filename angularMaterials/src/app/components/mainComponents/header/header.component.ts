ba11efccaae031e5d62b4be2d95e3e5f235d5084
BartlomiejDarocha <bartlomiejdarocha@gmail.com>
2019-12-08 21:05:58 +0100

    side menu

--- angularMaterials/src/app/components/mainComponents/header/header.component.ts
+++ angularMaterials/src/app/components/mainComponents/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatSidenav } from '@angular/material';
 
 @Component({
   selector: 'app-header',
@@ -6,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.less']
 })
 export class HeaderComponent implements OnInit {
-
+  @ViewChild('sidenav') sidenav: MatSidenav;
   constructor() { }
 
   ngOnInit() {
+  }
+  close() {
+    this.sidenav.close();
   }
 }
