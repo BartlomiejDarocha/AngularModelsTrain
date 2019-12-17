9f0859e3330a9daafc8ea168f67e2a07fccdac16
BartlomiejDarocha <bartlomiejdarocha@gmail.com>
2019-12-17 20:54:37 +0100

    http try promise

--- angularMaterials/src/app/components/angular/promise/promise.component.ts
+++ angularMaterials/src/app/components/angular/promise/promise.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-promise',
@@ -8,20 +9,21 @@ import { Component, OnInit } from '@angular/core';
 export class PromiseComponent implements OnInit {
   ///// first Example
   public simplePromisResult = 'test';
+  public testUrl = 'https://swapi.co/api/people';
   public simplePromose = new Promise((resolves, rejects) => {
-    const mathTest = Math.floor(Math.random() * 8) + 2 ;
+    const mathTest = Math.floor(Math.random() * 8) + 2;
     setTimeout(() => {
       if (mathTest >= 5) {
         resolves(`Succes promise result is ${mathTest}`);
       } else {
-        rejects( `fail promise result is ${mathTest}`);
+        rejects(`fail promise result is ${mathTest}`);
       }
     }, 500);
   });
   //////
   ///// sec Example
   promiseCount = 0;
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   public ngOnInit(): void {
   }
@@ -32,15 +34,15 @@ export class PromiseComponent implements OnInit {
         console.log(typeof data, data);
         this.simplePromisResult = `${data}`;
       }).catch((datafail) => {
-          console.log(typeof datafail, datafail);
-          this.simplePromisResult = `${datafail}`;
+        console.log(typeof datafail, datafail);
+        this.simplePromisResult = `${datafail}`;
       });
     }
   }
-/// sec example
+  /// sec example
   public secExample(): void {
     const newPromiseCounter = ++this.promiseCount;
-    const promise1 =  new Promise((resolves, rejects) => {
+    const promise1 = new Promise((resolves, rejects) => {
       setTimeout(() => {
         if (newPromiseCounter !== 10) {
           resolves(newPromiseCounter);
@@ -56,5 +58,22 @@ export class PromiseComponent implements OnInit {
     });
     console.log(newPromiseCounter, 'newPromiseCounter last');
   }
+  /// http example asynchonic on promise
+  /// promise
+  public async getStarWarsBypromiseSimple(url: string) {
+    return await this.http.get(url).toPromise();
+  }
 
+  public testGeOnTry() {
+    try {
+      this.getStarWarsBypromiseSimple('https://swapi.co/api/people').then(result => {
+        console.log(result, 'result then');
+      }).catch(error => {
+        console.log(error, 'ERROR');
+      });
+    } catch (error) {
+      console.log(error, '22222');
+    }
+    return;
+  }
 }
