6c64c1afac37fb54320987d4458c5bdd9a9e5876
Bartlap <bartlomiejdarocha@gmail.com>
2019-12-28 10:53:57 +0100

    TaskList variables and Inputs, outPusts

--- angularMaterials/src/app/components/angular/services/components/taskslist/taskslist.component.ts
+++ angularMaterials/src/app/components/angular/services/components/taskslist/taskslist.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output } from '@angular/core';
+import { TaskMedium } from 'src/app/models/tasks.models';
+import { EventEmitter } from 'protractor';
 
 @Component({
   selector: 'app-taskslist',
@@ -6,7 +8,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./taskslist.component.less']
 })
 export class TaskslistComponent implements OnInit {
-
+  @Input() public taskList: Array<TaskMedium>;
+  @Output() private taskListMod = new EventEmitter();
+  public colorList = ['red', 'green', 'yellow', 'gray', 'orange', 'silver'];
+  public rateList = [0, 1, 2, 3, 4 , 5];
   constructor() { }
 
   ngOnInit() {
