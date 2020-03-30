a09e315705d696b08313f3024318cfaa352c65be
BartlomiejDarocha <bartlomiejdarocha@gmail.com>
2020-03-30 20:08:36 +0200

    geterSeterCompleate

--- angularMaterials/src/app/providers/getter-setter.service.ts
+++ angularMaterials/src/app/providers/getter-setter.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { ApiService } from './api.service';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,6 +10,18 @@ export class GetterSetterService {
 
   constructor(private apiService: ApiService) { }
 
-  
+  public loadData(): Observable <any> {
+    return this.apiService.get('https://swapi.co/api/people');
+  }
+
+  public setStarWarsPeople(data: any): void {
+    this.setStarWarsPeople = data;
+    console.log(this.setStarWarsPeople, 'this.setStarWarsPeople');
+  }
+
+  public getStarWarsPeople(): any {
+    return this.setStarWarsPeople;
+  }
+
 
 }
