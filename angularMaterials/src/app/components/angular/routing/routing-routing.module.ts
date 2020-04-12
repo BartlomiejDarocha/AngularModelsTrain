1126e2cebaf028a4632280db4e14fee87461cd28
Bartlap <bartlomiejdarocha@gmail.com>
2020-04-12 15:00:31 +0200

    Testing routing routing module

--- angularMaterials/src/app/components/angular/routing/routing-routing.module.ts
+++ angularMaterials/src/app/components/angular/routing/routing-routing.module.ts
@@ -1,5 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AngularHomeComponent } from '../angular-home/angular-home.component';
+import { RoutingComponent } from './routing.component';
+import { OneLvlRotuingComponent } from './one-lvl-rotuing/one-lvl-rotuing.component';
+import { TwolvlRoutingComponent } from './twolvlRouting/twolvl-routing/twolvl-routing.component';
+import { GrandChildComponent } from './twolvlRouting/grand-child/grand-child.component';
 
 // to będzie jako 2 opcja routingu tak by było to lepsze do update kodu
 const routes: Routes = [
