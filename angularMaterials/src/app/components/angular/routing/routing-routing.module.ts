54c7e7e793aed7116ddd38c2011a2742fde51244
Bartlap <bartlomiejdarocha@gmail.com>
2020-04-12 14:32:49 +0200

    routing module  type 2

--- angularMaterials/src/app/components/angular/routing/routing-routing.module.ts
+++ angularMaterials/src/app/components/angular/routing/routing-routing.module.ts
@@ -3,6 +3,23 @@ import { Routes, RouterModule } from '@angular/router';
 
 // to będzie jako 2 opcja routingu tak by było to lepsze do update kodu
 const routes: Routes = [
+  {
+    path: 'angular', component: AngularHomeComponent,
+    children: [
+      {
+        path: 'routing', component: RoutingComponent,
+        children: [
+          { path: 'onelvl/:id', component: OneLvlRotuingComponent },
+          {
+            path: 'twolvl/:id', component: TwolvlRoutingComponent,
+            children: [
+              { path: 'twolvlGrand/:id', component: GrandChildComponent }
+            ]
+          }
+        ]
+      }
+    ]
+  }
 ];
 
 @NgModule({
