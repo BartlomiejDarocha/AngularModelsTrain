d9450e08af323c60031ecf8f673630099bfbfb67
LAPTOP\Bart <bartlomiejdarocha@gmail.com>
2020-11-16 23:24:33 +0100

    budowanie derektywy awatar

--- angularMaterials/src/app/directives/avatar.directive.ts
+++ angularMaterials/src/app/directives/avatar.directive.ts
@@ -1,10 +1,22 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, Input, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appAvatar]'
 })
-export class AvatarDirective {
+export class AvatarDirective implements OnInit {
+  @Input() name = '';
+  @Input() imgSrc = '';
+  private imgBox: HTMLElement;
 
-  constructor() { }
+  constructor(
+    private renderer: Renderer2,
+    private element: ElementRef
+  ) {
+    this.imgBox = this.renderer.createElement('img');
+    console.log(this.imgBox, 'imgBox');
+   }
+
+  ngOnInit(): void {
+  }
 
 }
