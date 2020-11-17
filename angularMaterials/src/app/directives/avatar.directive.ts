8067fdaa907a3ba6bfaacb2ef72af2d158273d2c
LAPTOP\Bart <bartlomiejdarocha@gmail.com>
2020-11-17 23:04:13 +0100

    dodawanie styli do derektywy

--- angularMaterials/src/app/directives/avatar.directive.ts
+++ angularMaterials/src/app/directives/avatar.directive.ts
@@ -15,17 +15,19 @@ export class AvatarDirective implements OnInit {
     private renderer: Renderer2,
     private element: ElementRef
   ) {
+    this.element.nativeElement.classList.add('avatarDirectiveTest');
     this.imgBox = this.renderer.createElement('img');
-    this.imgBox = this.renderer.createElement('div');
+    this.divName = this.renderer.createElement('div');
+    this.divName.classList.add('avatarNoImg');
    }
 
   ngOnInit(): void {
-    // if (this.imgSrc) {
-    //   this.renderer.setAttribute(this.imgBox, 'src', this.imgSrc);
-    //   this.renderer.appendChild(this.element.nativeElement, this.imgBox);
-    //   return;
-    // }
-    const randomIndex = Math.ceil(Math.random() * this.backGroundColors.length - 1);
+    if (this.imgSrc) {
+      this.renderer.setAttribute(this.imgBox, 'src', this.imgSrc);
+      this.renderer.appendChild(this.element.nativeElement, this.imgBox);
+      return;
+    }
+    const randomIndex = Math.round(Math.random() * (this.backGroundColors.length - 1));
     console.log(randomIndex, 'randomIndex');
   }
 
