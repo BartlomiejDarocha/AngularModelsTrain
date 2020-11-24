1802da8f569e634873c16e2eb7fd7c3fd09148bd
LAPTOP\Bart <bartlomiejdarocha@gmail.com>
2020-11-24 21:09:04 +0100

    train extends class

--- angularMaterials/src/app/components/angular/routing-train-angular/components/action-bar/action-bar.component.ts
+++ angularMaterials/src/app/components/angular/routing-train-angular/components/action-bar/action-bar.component.ts
@@ -1,5 +1,14 @@
 import { Component, OnInit, EventEmitter, Output, Input } from '@angular/core';
 
+// class extend train
+export class Animal {
+  constructor() {
+  }
+}
+export class MyPet {
+  
+}
+
 export interface BarOptionsInterface {
   id: number;
   name: string;
