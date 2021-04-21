5448cc99ae27f9e197ec80ac7eae78f15f370b6c
LAPTOP\Bart <bartlomiejdarocha@gmail.com>
2021-04-21 22:25:22 +0200

    Jakieś zmiany

--- angularMaterials/src/app/components/dialogs/dialogsTemplates/input-dialog/input-dialog.component.ts
+++ angularMaterials/src/app/components/dialogs/dialogsTemplates/input-dialog/input-dialog.component.ts
@@ -8,11 +8,14 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
   styleUrls: ['./input-dialog.component.less']
 })
 export class InputDialogComponent {
+  public dataDialog: NameAndColor;
 
   constructor(
     public dialogRef: MatDialogRef<InputDialogComponent>, // zwraca dane z inputa itp
     @Inject(MAT_DIALOG_DATA) public data: NameAndColor // przyjmuje dane najlepiej model
-    ) {}
+    ) {
+      this.dataDialog = data;
+    }
 
   onNoClick(): void {
     this.dialogRef.close();
